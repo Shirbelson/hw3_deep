56addc334f1d5511890a90c7c6d1119b08fadac0
Shirbelson <shir@ShirBelson.localdomain>
2025-12-30 17:34:18 +0200

    open Questions part 1

--- hw3/answers.py
+++ hw3/answers.py
@@ -38,19 +38,34 @@ def part1_generation_params():
 
 
 part1_q1 = r"""
-**Your answer:**
+We split the corpus into sequences instead of training on the whole text mainly for memory reasons, training on the entire corpus requires a lot of memory, as the length of the sequence increases, the consumption increases, and therefore splitting into short sequences allows for efficient training within memory limitations.
+Training in smaller parts addresses the problems of exploding and vanishing gradients that we saw in the past by keeping them within a range of values ​​that allows for effective updating of the weights.
+In addition, by dividing into mini-batches we allow the model to perform frequent updates after each batch, thus enabling convergence to an optimal solution more accurately and faster than by performing a single update at the end of the corpus.
+
 """
 
 part1_q2 = r"""
-**Your answer:**
+It possible that the generated text shows memory longer than the sequence length Because Instead of reinitializing the model for each sequence, we use a hidden state from the end of one sequence as the initial state for the next one. 
+This allows the hidden state to act as persistent memory, the model learns over time and well beyond the length of a single training sequence.
+
 """
 
 part1_q3 = r"""
-**Your answer:**
+We not shuffling the order of batches when training Because, as we explained in the previous section, the hidden state moves from the end of a sequence to the beginning of the next sequence, and therefore continuity of information is required. If we shuffle the data, the model will not be able to learn the connections between the parts of the sentence,
+and when we move the hidden state, the memory it holds will not teach our model with the level of accuracy and predictions we expected.
+
 """
 
 part1_q4 = r"""
-**Your answer:**
+1. We lower the temperature for sampling Because It makes the model more conservative. By dividing the logits by a number less than 1, we sharpen the gaps between the logit values ​​and therefore also sharpen the probability distribution during the Softmax. 
+The model gives higher weight to its safe option, thus reducing randomness and increasing the chance of getting a more accurate and consistent answer.
+
+2. When the temperature is very high Because The model becomes more adventurous and less stable. Dividing the logit by a larger number flattens the probability distribution so that the gaps between the words are reduced, as is their softmax difference, and the model will ignore its preferences. Therefore, with a uniform probability distribution,
+ we would expect more random and less accurate results, since the words get almost an equal chance of being selected, making it more uniform.And a less logically sensible output is expected.
+
+3. When the temperature is very low Because The model becomes more repetitive and deterministic. Dividing the logits by a very small value increases the gaps and sharpens the probability distribution during their Softmax step, forcing the model to choose its safest option (the one with the highest score) and ignore the others. 
+We expect a more logical and accurate output, but also repetitive and uncreative.
+
 """
 # ==============
 
